1697e0bed6a2f104d506c3f3b9f6c82be401a7f2
Ravi Kiran J <rjravikiran95@gmail.com>
2026-09-21 09:09:46 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const mapping = { ")": "(", "}": "{", "]": "[" };
+        for (const char of s) {
+            if (char in mapping) {
+                if (stack.length === 0 || stack[stack.length - 1] !== mapping[char]) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+
+        } 
+        return stack.length === 0;
+    }
+}
